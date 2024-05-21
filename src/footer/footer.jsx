133f1e44ba0832ba46e436cbb70f1fe396cc5a18
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-05-21 19:37:29 +0530

    us001:footer initilized

---
+++ src/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './footer.module.css'
+const Footer = () => {
+  return (
+    <Footer>
+        &copy;2024 all rights reserved by rh-comrade
+    </Footer>
+  )
+}
+
+export default Footer
